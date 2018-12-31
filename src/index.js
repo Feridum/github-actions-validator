deba445fe8c589e6396012b9f29dbadd0038d4af
feridum <aleksander.patschek@gmail.com>
2018-12-31 17:26:53 +0100

    isWorkflowValid will always return boolean value

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-const {demoWorkflow} = require('../demo/demo.workflow')
+
 const {parseWorkflow} = require('./parse-workflow/parse-workflow')
 const {validateWorkflow} = require('./validate-workflow-file/validate-workflow')
 
 
 
 const isWorkflowValid = (workflow) => {
-    const parsedWorkflow = parseWorkflow(workflow)
-    const validation = validateWorkflow(parsedWorkflow)
-
-    return validation
+    try{
+        const parsedWorkflow = parseWorkflow(workflow)
+        const validation = validateWorkflow(parsedWorkflow)
+        return validation;
+    }catch(e){
+        return false
+    }
+    
 }
 
 module.exports = {
